f89b0c36c4b702b397cd5b9d96b9ab2d1d79c057
libertyhound <scdgalindo@gmail.com>
2021-01-21 16:48:58 -0600

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -8,5 +8,6 @@ apt update
 apt-cache policy docker-ce
 apt install docker-ce
 docker run hello-world
-systemctl status docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 EOF
