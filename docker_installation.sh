f14091cbd2453202658fb4bce8b9a706f43ecdd7
libertyhound <scdgalindo@gmail.com>
2021-01-25 16:21:33 -0600

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -6,8 +6,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt install docker-ce -y
 docker run hello-world
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker --version
+docker-compose --version
 EOF
